acf5f690fe9f281fa997e0633b78b9f1e4365ac1
Ismail Dad Khan <ismaildadkhan@gmail.com>
2025-01-20 21:14:12 +0500

    Create age_difference_calculator.py

---
+++ age_difference_calculator.py
@@ -0,0 +1,6 @@
+# Program to calculate the age difference
+age1 = int(input("Enter the age of Person 1: "))
+age2 = int(input("Enter the age of Person 2: "))
+
+difference = abs(age1 - age2)
+print(f"The age difference between Person 1 and Person 2 is: {difference} years.")
