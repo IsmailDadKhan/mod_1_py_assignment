137e9780e3832d80807931b7070b0436c3d8a6e8
Ismail Dad Khan <ismaildadkhan@gmail.com>
2025-01-20 21:00:55 +0500

    Create custom_message_formatter.py

---
+++ custom_message_formatter.py
@@ -0,0 +1,6 @@
+# Program to display a custom message
+name = input("Enter your name: ")
+color = input("Enter your favorite color: ")
+number = input("Enter your favorite number: ")
+
+print(f"{name} loves the color {color} and their favorite number is {number}.")
